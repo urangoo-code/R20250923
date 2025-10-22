13eb5fcd4da38510669fbb87903ba4ee30b596fa
Urangoo <chinbayarurangoo@gmail.com>
2025-10-22 19:37:16 +0800

    add real information

--- Practise/SQLQuery_1.sql
+++
@@ -1,8 +0,0 @@
--- Create a table called Students
-CREATE TABLE Students (
-    ID INT IDENTITY(1,1) PRIMARY KEY,   -- Auto-incrementing ID
-    Name NVARCHAR(50) NOT NULL,          -- Name column, up to 50 characters
-    Age INT NOT NULL,                     -- Age column
-    EnrollmentDate DATE DEFAULT GETDATE() -- Optional default value
-);
-GO
